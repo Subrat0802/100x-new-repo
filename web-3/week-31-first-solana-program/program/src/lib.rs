9af2e47db499ad9f8f943ab98e86ab4f79fddc19
Subrat Mishra <piyush.subrat8@gmail.com>
2025-09-30 00:51:19 +0530

    add

--- web-3/week-31-first-solana-program/program/src/lib.rs
+++ web-3/week-31-first-solana-program/program/src/lib.rs
@@ -1,51 +1,78 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint,
-    entrypoint::ProgramResult,
-    msg,
-    pubkey::Pubkey,
-};
+import { expect, test } from "bun:test";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import * as borsh from "borsh";
+import { COUNTER_SIZE, schema } from "./types"; // your Borsh schema
 
-entrypoint!(process_instruction);
+const adminAccount = Keypair.generate();
+const dataAccount = Keypair.generate();
+const connection = new Connection("http://127.0.0.1:8899");
 
-#[derive(BorshSerialize, BorshDeserialize)]
-enum CounterInstruction {
-    Increment(u32),
-    Decrement(u32),
-}
+const PROGRAM_ID = new PublicKey("CKHqwxScnXGAbMJZtYJ4AwAcmQBABSmHtWtoT1gmB6Uh");
 
-#[derive(BorshSerialize, BorshDeserialize)]
-struct Counter {
-    count: u32,
-}
+test("Initialize counter account", async () => {
+  // Airdrop some SOL to admin
+  const txn = await connection.requestAirdrop(adminAccount.publicKey, 1_000_000_000);
+  await connection.confirmTransaction(txn);
 
-pub fn process_instruction(
-    _program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    let account = next_account_info(&mut accounts.iter())?;
-    
-    // Deserialize the counter
-    let mut counter = Counter::try_from_slice(&account.data.borrow())?;
-    
-    // Deserialize the instruction
-    let instruction = CounterInstruction::try_from_slice(instruction_data)?;
-    
-    match instruction {
-        CounterInstruction::Increment(amount) => {
-            counter.count += amount;
-        }
-        CounterInstruction::Decrement(amount) => {
-            counter.count -= amount;
-        }
-    }
+  // Create the counter account
+  const lamports = await connection.getMinimumBalanceForRentExemption(COUNTER_SIZE);
+  const ix = SystemProgram.createAccount({
+    fromPubkey: adminAccount.publicKey,
+    newAccountPubkey: dataAccount.publicKey,
+    lamports,
+    space: COUNTER_SIZE,
+    programId: PROGRAM_ID,
+  });
 
-    // Serialize the updated counter back
-    counter.serialize(&mut *account.data.borrow_mut())?;
-    
-    msg!("Counter updated to {}", counter.count);
+  const createTxn = new Transaction().add(ix);
+  await connection.sendTransaction(createTxn, [adminAccount, dataAccount]);
 
-    Ok(())
-}
+  // Initialize counter to 0 using the program
+  const initIx = new TransactionInstruction({
+    keys: [{ pubkey: dataAccount.publicKey, isSigner: false, isWritable: true }],
+    programId: PROGRAM_ID,
+    data: Buffer.from([0]), // 0 = Initialize instruction in Rust
+  });
+
+  await connection.sendTransaction(new Transaction().add(initIx), [adminAccount]);
+
+  // Check initial counter
+  const info = await connection.getAccountInfo(dataAccount.publicKey);
+  const counter = borsh.deserialize(schema, info!.data);
+  expect(counter.count).toBe(0);
+});
+
+test("Increment counter", async () => {
+  const incrementIx = new TransactionInstruction({
+    keys: [{ pubkey: dataAccount.publicKey, isSigner: false, isWritable: true }],
+    programId: PROGRAM_ID,
+    data: Buffer.from([1, 0, 0, 0, 1]), // 1 = Increment, followed by u32 amount = 1
+  });
+
+  await connection.sendTransaction(new Transaction().add(incrementIx), [adminAccount]);
+
+  const info = await connection.getAccountInfo(dataAccount.publicKey);
+  const counter = borsh.deserialize(schema, info!.data);
+  expect(counter.count).toBe(1);
+});
+
+test("Decrement counter", async () => {
+  const decrementIx = new TransactionInstruction({
+    keys: [{ pubkey: dataAccount.publicKey, isSigner: false, isWritable: true }],
+    programId: PROGRAM_ID,
+    data: Buffer.from([2, 0, 0, 0, 1]), // 2 = Decrement, u32 amount = 1
+  });
+
+  await connection.sendTransaction(new Transaction().add(decrementIx), [adminAccount]);
+
+  const info = await connection.getAccountInfo(dataAccount.publicKey);
+  const counter = borsh.deserialize(schema, info!.data);
+  expect(counter.count).toBe(0);
+});
---
+++ web-3/week-38-cal-anchor/calculator/programs/calculator/src/lib.rs
@@ -0,0 +1,54 @@
+use anchor_lang::prelude::*;
+
+declare_id!("3DGAwR8pv7N8QAhs29ZeidA219Bgs8jvpASL5mibSRh7");
+
+#[program]
+pub mod calculator {
+    use super::*;
+
+    pub fn init(ctx: Context<Initialize>, init_value: u32) -> Result<()>{
+        ctx.accounts.account.num = init_value;
+        Ok(())
+    }
+
+    pub fn double(ctx: Context<Double>) -> Result<()>{
+        ctx.accounts.account.num *= 2;
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Add>, num: u32) -> Result<()> {
+        ctx.accounts.account.num += num;
+        Ok(())
+    }
+}
+
+#[account]
+struct DataShape {
+    pub num: u32
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub account: Account<'info, DataShape>,
+    pub system_program: Program<'info, System>,
+    #[account(mut)]
+    pub signer: Signer<'info>
+}
+
+
+#[derive(Accounts)]
+pub struct Double<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct Add<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>
+}
